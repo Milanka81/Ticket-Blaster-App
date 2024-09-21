af42278ab424e8893f0866b4b6fa7ed63cfc8afc
milanka <mina.m81@gmail.com>
2024-09-21 22:14:35 +0200

    header and footer component styled and created

--- clientReact/src/Components/Navbar/Navbar.tsx
+++ clientReact/src/Components/Navbar/Navbar.tsx
@@ -1,13 +1,45 @@
 import { NavLink } from "react-router-dom";
+import { useNavigate } from "react-router";
+import styles from "./Navbar.module.css";
 
-const Navbar = () => {
+const Navbar = ({ role }: { role: string }) => {
+  const navigate = useNavigate();
   return (
-    <div>
-      <NavLink to="/">Home</NavLink>
-      <NavLink to="/concerts">Musical Concerts</NavLink>
-      <NavLink to="/stand-ups">Stand-up Comedy</NavLink>
-      <NavLink to="/login">Login</NavLink>
-      <NavLink to="/register">Create Account</NavLink>
+    <div className={styles.navbar}>
+      <div className={styles.navbarContainer}>
+        <NavLink className={styles.logo} to="/">
+          <img src="/img/Path 1.svg"></img>
+        </NavLink>
+        <NavLink className={styles.navlinks} to="/events">
+          Musical Concerts
+        </NavLink>
+        <NavLink className={styles.navlinks} to="/events">
+          Stand-up Comedy
+        </NavLink>
+        {role === "header" ? (
+          <div className={styles.searchBtnsContainer}>
+            <input
+              className={styles.searchbar}
+              type="text"
+              placeholder="Search"
+            />
+            <button
+              className={styles.btnLogin}
+              onClick={() => navigate("/login")}
+            >
+              Log in
+            </button>
+            <button
+              className={styles.btnCreateAccount}
+              onClick={() => navigate("/register")}
+            >
+              Create Account
+            </button>
+          </div>
+        ) : (
+          <p className={styles.copyright}>®Copyright TicketBlaster 2004</p>
+        )}
+      </div>
     </div>
   );
 };
